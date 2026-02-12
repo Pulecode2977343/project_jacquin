75e7088a6ddd875a345bb969938ff920fdedf667
Manuel Pertuz <>
2026-02-12 14:26:29 -0500

    chore: exclude non-essential files and folders from repository

--- sync_fix.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-copy /Y "d:\Documentos\GitHub\project_jacquin\web_page\pages\gestion.html" "c:\xampp\htdocs\jacquin_web\pages\gestion.html"
-copy /Y "d:\Documentos\GitHub\project_jacquin\web_page\pages\js\dashboard.js" "c:\xampp\htdocs\jacquin_web\pages\js\dashboard.js"
-echo Sync Complete

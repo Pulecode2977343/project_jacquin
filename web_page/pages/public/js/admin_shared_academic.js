e4a1213aec6c26672b7a56e448621ad11addfd9f
modusaxon-hub <modusaxon@gmail.com>
2026-03-02 18:54:08 -0500

    chore: Eliminado un archivo.

--- web_page/pages/public/js/admin_shared_academic.js
+++
@@ -1,7 +0,0 @@
-/**
- * DEPRECATED: This file has been superseded by admin_academic_schedules.js which now contains the unified AcademicManager.
- * 
- * Please do not use functions from this file. 
- * Ensure your HTML refers to window.openCourseManagement which is now bound in admin_academic_schedules.js
- */
-console.log("admin_shared_academic.js is deprecated. Use AcademicManager in admin_academic_schedules.js instead.");
